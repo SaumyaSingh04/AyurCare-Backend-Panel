7d58b8e97a4f0c9016ba565552a4b97875127583
Anshu Sharma <Anshusharma42019@gmail.com>
2026-06-06 14:45:22 +0530

    Update CORS origin

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://ayur-care-frontend-panel.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
